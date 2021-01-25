94da4fca2a32038cabe410d6e4a2ea86b0f8d990
dhbhernandez <73495794+dhbhernandez@users.noreply.github.com>
2021-01-25 10:49:51 -0600

    containers update

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,12 +1,38 @@
 import React, { Component } from "react";
-import SearchArea from '/components/SearchArea';
-
+import SearchArea from './SearchArea';
+import request from 'superagent';
 
 class Books extends Component {
+    constructor(props){
+        super(props);
+        this.state= {
+            books: [],
+            searchField: ''
+
+        }
+    }
+
+    searchBook = (e) => {
+        e.preventDefault();
+        request
+        .get("https://www.googleapis.com/books/v1/volumes")
+        .query({q: this.state.searchField })
+        .then((data) => {
+            console.log(data);
+        })
+    }
+
+
+    handleSearch = (e) => {
+        // console.log(e.target.value)
+        this.setState({ searchField: e.target.value })
+    }
+
+
     render() {
         return (
             <div>
-                <SearchArea handleSearch={this.handleSearch} />
+                <SearchArea searchBook={this.searchBook} handleSearch={this.handleSearch} />
             </div>
         );
     }
